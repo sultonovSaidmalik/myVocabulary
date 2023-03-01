8a43f6f6edf2a80bfb4899f847acba03fd50a91b
sultonovSaidmalik <sultonov200v@gmail.com>
2023-03-01 15:39:21 +0500

    vocabulary new 1

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -1,6 +1,19 @@
-
 import '../model/vocabulary.dart';
 
 List<Vocabulary> vocabulary = [
-
+  Vocabulary(
+      id: 0,
+      english: 'english',
+      uzbek: 'uzbek',
+      createdAt: DateTime.now().toString()),
+  Vocabulary(
+      id: 1,
+      english: 'english',
+      uzbek: 'uzbek',
+      createdAt: DateTime.now().toString()),
+  Vocabulary(
+      id: 2,
+      english: 'english',
+      uzbek: 'uzbek',
+      createdAt: DateTime.now().toString()),
 ];
